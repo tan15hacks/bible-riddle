6c5efb9ff37bf81ae23382246785408bb0060a66
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-11 16:29:41 +0800

    Expand progress repository contract

--- lib/domain/repositories/progress_repository.dart
+++ lib/domain/repositories/progress_repository.dart
@@ -2,9 +2,11 @@ import '../entities/player_progress.dart';
 
 abstract interface class ProgressRepository {
   Future<LevelProgress?> getLevelProgress(String riddleId);
+  Future<Map<String, LevelProgress>> getAllLevelProgress();
   Future<void> saveLevelProgress(LevelProgress progress);
   Future<PlayerEconomy> getEconomy();
   Future<void> saveEconomy(PlayerEconomy economy);
   Future<PlayerStatistics> getStatistics();
   Future<void> saveStatistics(PlayerStatistics statistics);
+  Future<void> resetAllProgress();
 }
